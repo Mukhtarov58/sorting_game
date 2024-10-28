700aefeaa37385a7972358f7427dfba7609ae677
Timur <timur.pnz@mail.ru>
2024-10-28 18:24:55 +0300

    Изменил работу SoundPlayer

--- src/main/java/org/example/game/SoundPlayer.java
+++ src/main/java/org/example/game/SoundPlayer.java
@@ -4,13 +4,16 @@ package org.example.game;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.InputStream;
 
 public class SoundPlayer {
 
     public static void playSound(InputStream soundStream) {
-        try (AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundStream)) {
+        try (BufferedInputStream bufferedIn = new BufferedInputStream(soundStream);
+             AudioInputStream audioIn = AudioSystem.getAudioInputStream(bufferedIn)) {
+
             Clip clip = AudioSystem.getClip();
             clip.open(audioIn);
             clip.start();
